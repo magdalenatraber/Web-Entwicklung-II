6ccca2037c56da74fcef36cd1dad773319b18801
AKKA\Magdalena.TRABER <Magdalena.traber@akka.eu>
2021-09-22 10:23:55 +0200

    Test

--- HtmlJsTemplate/script/main.js
+++ HtmlJsTemplate/script/main.js
@@ -1,3 +1,10 @@
 "use strict";
 
 // hier kommen ihre Skripte hin
+const a = Math.Random();
+const b = Math.Random();
+let operand = prompt("Welcher Operator?+-/*");
+if (operand == "+") { funct add(a, b) { return a + b; }) }
+else if (operand == "-") { alert(funct substract(a, b) { return  a - b; }) }
+else if (operand == "*") { alert(funct multiply(a, b) { return a * b; }) }
+else if (operand == "/") { alert(funct multiply(a, b) { return a  b; }) }
