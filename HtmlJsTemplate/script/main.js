27e753fd5d7ac8b53dfb81a71cae80ca9274926e
Traber Magdalena <s44671@edu.campus02.at>
2021-09-23 10:15:35 +0200

    test

--- HtmlJsTemplate/script/main.js
+++ HtmlJsTemplate/script/main.js
@@ -1,10 +1,48 @@
 "use strict";
 
 // hier kommen ihre Skripte hin
-const a = Math.Random();
-const b = Math.Random();
-let operand = prompt("Welcher Operator?+-/*");
-if (operand == "+") { funct add(a, b) { return a + b; }) }
-else if (operand == "-") { alert(funct substract(a, b) { return  a - b; }) }
-else if (operand == "*") { alert(funct multiply(a, b) { return a * b; }) }
-else if (operand == "/") { alert(funct multiply(a, b) { return a  b; }) }
+const a = Math.random();
+const b = Math.random();
+
+console.log(a);
+console.log(b);
+
+const operation = prompt("Operation: +-*/");
+let result;
+switch (operation) {
+    case "+":
+        result = add(a, b);
+        break;
+
+    case "-":
+        result = sub(a, b);
+        break;
+
+    case "*":
+        result = mul(a, b);
+        break;
+
+    case "/":
+        result = div(a, b);
+        break;
+}
+
+console.log(result);
+
+function add(a, b) {
+    return a + b;
+}
+
+function sub(a, b) {
+    return a - b;
+}
+
+function mul(a, b) {
+    return a * b;
+}
+
+function div(a, b) {
+    return a / b;
+
+    
+}
