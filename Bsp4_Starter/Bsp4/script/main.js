3d2b8e4473eaec785f819cf57c625523c70f3e37
Traber Magdalena <s44671@edu.campus02.at>
2021-09-24 12:25:39 +0200

    Bsp 4.3 fertig

--- Bsp4_Starter/Bsp4/script/main.js
+++ Bsp4_Starter/Bsp4/script/main.js
@@ -3,10 +3,19 @@ const baseUrl = "http://nowaunoweb.azurewebsites.net/Content/Cards/";
 
 // hier kommen ihre Skripte hin
 
-let button = document.getElementByTagName("button");
+let button = document.getElementsByTagName("button");
 
-button.addEventListener("click", ()=> {
+button[0].addEventListener("click", ()=> {
 let color = document.getElementById("color").value;
 let number = document.getElementById("number").value;
-let card = color + " " + number;
-(alert(card))});
+let card = color[0]  + number;
+
+let newElement = document.createElement("li");
+let newImg = document.createElement("img");
+newImg.src = `${baseUrl}${card}.png`;
+const liste = document.querySelector("#cards ul");
+
+const newlistitem = liste.appendChild(newElement);
+newlistitem.appendChild(newImg);
+
+});
