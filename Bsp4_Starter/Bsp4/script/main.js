055ee5b7ec2d72746dbc716fc876b8847823afb1
Traber Magdalena <s44671@edu.campus02.at>
2021-10-01 11:17:36 +0200

    Bsp4.4

--- Bsp4_Starter/Bsp4/script/main.js
+++ Bsp4_Starter/Bsp4/script/main.js
@@ -5,9 +5,13 @@ const baseUrl = "http://nowaunoweb.azurewebsites.net/Content/Cards/";
 
 
 
-let button = document.getElementsByTagName("button");
+let erstellen = document.getElementById("Erstellen");
+let löschen = document.getElementById("Löschen");
+let filterlöschen = document.getElementById("Filter-Löschen");
+const liste = document.querySelector("#cards ul");
 
-button[0].addEventListener("click", ()=> {
+erstellen.addEventListener("click", ()=> {
+
 let color = document.getElementById("color").value;
 let number = document.getElementById("number").value;
 let card = color[0]  + number;
@@ -15,9 +19,34 @@ let card = color[0]  + number;
 let newElement = document.createElement("li");
 let newImg = document.createElement("img");
 newImg.src = `${baseUrl}${card}.png`;
-const liste = document.querySelector("#cards ul");
+
 console.log(liste);
 const newlistitem = liste.appendChild(newElement);
 newlistitem.appendChild(newImg);
 
 });
+ löschen.addEventListener("click", ()=>{
+ let lastElement = liste.lastChild;
+ liste.removeChild(lastElement);
+ 
+ }
+ )
+ 
+
+ filterlöschen.addEventListener("click", ()=> {
+
+    let color = document.getElementById("color").value;
+    let number = document.getElementById("number").value;
+    let card = color[0]  + number;
+    console.log(liste.childNodes);
+    for (let index = 1; index < liste.childNodes.length; index++) {
+        let node =  liste.childNodes[index];
+        if(node.firstChild.src == `${baseUrl}${card}.png`){
+        liste.removeChild(node);
+        }
+        
+    }
+
+
+
+ })
