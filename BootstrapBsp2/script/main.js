ed56b8293a272404211dcbeac3dccb21fc578dc8
magdalena.traber <magdalena.traber@gmx.net>
2021-10-28 11:06:24 +0200

    ootstrap fertig und Animationen

--- BootstrapBsp2/script/main.js
+++ BootstrapBsp2/script/main.js
@@ -2,4 +2,5 @@
 const baseUrl = "http://nowaunoweb.azurewebsites.net/Content/Cards/";
 
 // hier kommen ihre Skripte hin
-var myModal = new bootstrap.Modal(document.getElementById('myModal')options)
+var myModal = new bootstrap.Modal(document.getElementById('playerNames'));
+myModal.show()
